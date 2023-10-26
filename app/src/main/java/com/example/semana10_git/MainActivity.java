a1d3450afbce0ce9ee511865629c1eb3e5384275
Laboratorio <j.galindo3@alumnos.santotomas.cl>
2023-10-26 16:38:00 -0300

    Hello Word

--- app/src/main/java/com/example/semana10_git/MainActivity.java
+++ app/src/main/java/com/example/semana10_git/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.sql.SQLOutput;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Hello Word!");
     }
 }
